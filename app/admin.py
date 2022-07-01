63b11bad5e6a47e933f16ddeb3c816d943dd3e56
Kuba <jakubpkrajewski@gmail.com>
2022-07-02 01:40:22 +0200

    webhook reciving try

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Task, Student, Lesson
+from .models import Task, Student, Lesson, FacebookMessage
 
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Task)
 admin.site.register(Lesson)
+admin.site.register(FacebookMessage)
