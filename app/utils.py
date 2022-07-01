9f50de9441142953943ce7ae93f0edf1f8ce4d55
Kuba <jakubpkrajewski@gmail.com>
2022-07-01 23:36:08 +0200

    views update fb webhook

--- app/utils.py
+++ app/utils.py
@@ -4,13 +4,15 @@ from django.conf import settings
 
 class FacebookMessengerAPI:
 
-    def get_verify_token(self):
+    @classmethod
+    def get_verify_token(cls):
         return settings.FACEBOOK_PAGE_VERIFY_TOKEN
 
-    def call_send(self, senderPsid, response):
+    @classmethod
+    def call_send(cls, sender_psid, response):
         page_access_token = settings.FACEBOOK_PAGE_ACCESS_TOKEN
         payload = {
-            'recipient': {'id': senderPsid},
+            'recipient': {'id': sender_psid},
             'message': response,
             'messaging_type': 'RESPONSE'
         }
@@ -19,14 +21,14 @@ class FacebookMessengerAPI:
         r = requests.post(url, json=payload, headers=headers)
         print(r.text)
 
-    # Function for handling a message from MESSENGER
-    def handle_message(self, sender_psid, received_message):
+    @classmethod
+    def handle_message(cls, sender_psid, received_message):
         # check if received message contains text
         if 'text' in received_message:
 
             response = {"text": 'You just sent: {}'.format(received_message['text'])}
 
-            self.call_send(sender_psid, response)
+            cls.call_send(sender_psid, response)
         else:
             response = {"text": 'This chatbot only accepts text messages'}
-            self.call_send(sender_psid, response)
+            cls.call_send(sender_psid, response)
